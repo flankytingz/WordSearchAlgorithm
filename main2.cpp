09dbfebe547bf3cc01ee936f0e0a448220d71ee1
Flanky <flankedgonerogue@gmail.com>
2022-12-18 17:11:29 +0500

    + Build columns complete + Check columns complete + Prints co-ordinates

--- main2.cpp
+++ main2.cpp
@@ -10,20 +10,25 @@ const int COLUMNS = 5;
 char grid[ROWS][COLUMNS];
 string rows[ROWS];
 string rowsR[ROWS];
+string columns[COLUMNS];
+string columnsR[COLUMNS];
 
 void getGrid();
 
 void printGrid();
 
+void buildColumns();
 void buildRows();
 
 void checkRows(const string& word, stringstream& ss);
+void checkColumns(const string& word, stringstream& ss);
 
 int main()
 {
     getGrid();
     printGrid();
     buildRows();
+    buildColumns();
 
     ifstream wordList = ifstream(R"(C:\Users\Flanky\CLionProjects\CS101_A3\wordlist.txt)");
     if (!wordList.is_open())
@@ -44,6 +49,7 @@ int main()
         }
 
         checkRows(word, ss);
+        checkColumns(word, ss);
 
         if (ss.str().length() > 0)
         {
@@ -55,6 +61,30 @@ int main()
     cout << "ENTER BACKSPACE TO EXIT" << endl;
     while ((char) getch() != '\b');
     return 0;
+}
+
+void checkColumns(const string& word, stringstream& ss) {
+    for (int i = 0; i < COLUMNS; i++)
+    {
+        if (columns[i].find(word) != string::npos)
+        {
+            ss
+            << "Found in column " << i + 1 << " "
+            << "(" << columns[i].find(word) + 1 << ", " << i + 1 << ")"
+            << " -> "
+            << "(" << columns[i].find(word) + word.length() << ", " << i + 1 << ")"
+            << endl;
+        }
+        if (columnsR[i].find(word) != string::npos)
+        {
+            ss
+            << "Found reversed in column " << i + 1 << " "
+            << "(" << ROWS - (columns[i].find(word) + 1) << ", " << i + 1 << ")"
+            << " -> "
+            << "(" << ROWS - (columns[i].find(word) + word.length()) << ", " << i + 1 << ")"
+            << endl;
+        }
+    }
 }
 
 void checkRows(const string& word, stringstream& ss) {
@@ -81,6 +111,21 @@ void checkRows(const string& word, stringstream& ss) {
     }
 }
 
+void buildColumns()
+{
+    for (int i = 0; i < COLUMNS; i++)
+    {
+        for (int j = 0; j < ROWS; j++) // NOLINT(modernize-loop-convert)
+        {
+            columns[i] += grid[j][i];
+        }
+        for (int j = ROWS - 1; j >= 0; j--)
+        {
+            columnsR[i] += grid[j][i];
+        }
+    }
+}
+
 void buildRows() {
     for (int i = 0; i < ROWS; i++)
     {
@@ -88,10 +133,6 @@ void buildRows() {
         {
             rows[i] += grid[i][j];
         }
-    }
-
-    for (int i = 0; i < ROWS; i++)
-    {
         for (int j = COLUMNS - 1; j >= 0; j--)
         {
             rowsR[i] += grid[i][j];
